e6a1469723260d1acea93cc0599e21aa9a005857
Blackfish Studio <blackfish.cbba@gmail.com>
2021-12-27 14:35:18 -0400

    planta baja bueno

---
+++ layama.scene.js
@@ -0,0 +1,110 @@
+// Created with Motiva Layama v1.5 https://www.motivacg.com/layama
+
+function getLayamaCameras()
+{
+   var layamaCameras = new BABYLON.SmartArray(0);
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0000", a: "PhysCamera_Laya_General_001", p: new BABYLON.Vector3(2787.63, 150, 2952.85), l: new BABYLON.Vector3(2788.61, 150, 2952.67)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0001", a: "PhysCamera_Laya_General_002", p: new BABYLON.Vector3(3100.75, 150, 3003.08), l: new BABYLON.Vector3(3101.73, 150, 3002.9)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0002", a: "PhysCamera_Laya_General_003", p: new BABYLON.Vector3(3346, 150, 2843.53), l: new BABYLON.Vector3(3347, 150, 2843.44)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0003", a: "PhysCamera_Laya_General_004", p: new BABYLON.Vector3(3304.23, 150, 3140.52), l: new BABYLON.Vector3(3304.41, 150, 3141.5)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0004", a: "PhysCamera_Laya_General_005", p: new BABYLON.Vector3(3379.03, 150, 3462.38), l: new BABYLON.Vector3(3379.94, 150, 3462.81)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0005", a: "PhysCamera_Laya_General_006", p: new BABYLON.Vector3(3633.26, 250, 3699.35), l: new BABYLON.Vector3(3634.08, 250, 3698.78)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0006", a: "PhysCamera_Laya_General_007", p: new BABYLON.Vector3(3930.41, 150, 3787.71), l: new BABYLON.Vector3(3930.98, 150, 3788.53)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0007", a: "PhysCamera_Laya_General_008", p: new BABYLON.Vector3(4278.29, 150, 3781.25), l: new BABYLON.Vector3(4278.72, 150, 3782.15)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0008", a: "PhysCamera_Laya_General_009", p: new BABYLON.Vector3(4552.8, 150, 3513.09), l: new BABYLON.Vector3(4553.38, 150, 3512.27)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0009", a: "PhysCamera_Laya_General_010", p: new BABYLON.Vector3(4628.82, 150, 3192.01), l: new BABYLON.Vector3(4628.65, 150, 3191.03)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0010", a: "PhysCamera_Laya_General_011", p: new BABYLON.Vector3(4607.7, 150, 2861.17), l: new BABYLON.Vector3(4607.05, 150, 2860.41)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0011", a: "PhysCamera_Laya_General_013", p: new BABYLON.Vector3(4286.7, 150, 2667.53), l: new BABYLON.Vector3(4286.61, 150, 2668.53)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0012", a: "PhysCamera_Laya_General_014", p: new BABYLON.Vector3(4236.91, 30, 2839.27), l: new BABYLON.Vector3(4236.91, 30, 2840.27)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0013", a: "PhysCamera_Laya_General_015", p: new BABYLON.Vector3(4302.9, 30, 3051.06), l: new BABYLON.Vector3(4302.4, 30, 3051.93)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0014", a: "PhysCamera_Laya_General_016", p: new BABYLON.Vector3(4188.24, 30, 3240.93), l: new BABYLON.Vector3(4187.82, 30, 3240.02)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0015", a: "PhysCamera_Laya_General_017", p: new BABYLON.Vector3(4089.96, 30, 3433.79), l: new BABYLON.Vector3(4089.46, 30, 3434.65)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0016", a: "PhysCamera_Laya_General_018", p: new BABYLON.Vector3(3866.35, 30, 3514.9), l: new BABYLON.Vector3(3865.85, 30, 3515.77)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0017", a: "PhysCamera_Laya_General_019", p: new BABYLON.Vector3(3700.89, 30, 3618.76), l: new BABYLON.Vector3(3700.46, 30, 3619.66)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0018", a: "PhysCamera_Laya_General_020", p: new BABYLON.Vector3(3572.51, 30, 3858.05), l: new BABYLON.Vector3(3572.33, 30, 3859.04)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0019", a: "PhysCamera_Laya_General_021", p: new BABYLON.Vector3(3485.55, 150, 4065.59), l: new BABYLON.Vector3(3485.28, 150, 4066.56)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0020", a: "PhysCamera_Laya_General_022", p: new BABYLON.Vector3(3429.48, 150, 4211.52), l: new BABYLON.Vector3(3428.78, 150, 4210.8)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0021", a: "PhysCamera_Laya_General_023", p: new BABYLON.Vector3(3460.05, 150, 4351.79), l: new BABYLON.Vector3(3460.55, 150, 4352.66)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0022", a: "PhysCamera_Laya_General_024", p: new BABYLON.Vector3(3613.3, 150, 4552.3), l: new BABYLON.Vector3(3613.2, 150, 4551.3)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0023", a: "PhysCamera_Laya_General_025", p: new BABYLON.Vector3(3199.56, 150, 4145.39), l: new BABYLON.Vector3(3200.55, 150, 4145.55)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0024", a: "PhysCamera_Laya_General_026", p: new BABYLON.Vector3(3017.11, 150, 4042.96), l: new BABYLON.Vector3(3016.93, 150, 4041.97)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0025", a: "PhysCamera_Laya_General_027", p: new BABYLON.Vector3(3059.1, 150, 3713.6), l: new BABYLON.Vector3(3059.54, 150, 3712.71)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0026", a: "PhysCamera_Laya_General_028", p: new BABYLON.Vector3(3091.95, 150, 3428.93), l: new BABYLON.Vector3(3092.68, 150, 3428.24)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0027", a: "PhysCamera_Laya_General_029", p: new BABYLON.Vector3(3486.57, 150, 3354.1), l: new BABYLON.Vector3(3487.41, 150, 3353.56)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0028", a: "PhysCamera_Laya_General_030", p: new BABYLON.Vector3(3758.86, 150, 3232.71), l: new BABYLON.Vector3(3759.61, 150, 3232.05)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0029", a: "PhysCamera_Laya_General_031", p: new BABYLON.Vector3(3884.16, 150, 3000.4), l: new BABYLON.Vector3(3884.59, 150, 2999.5)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0030", a: "PhysCamera_Laya_General_032", p: new BABYLON.Vector3(3918.53, 150, 2781.04), l: new BABYLON.Vector3(3918.44, 150, 2780.05)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0031", a: "PhysCamera_Laya_General_033", p: new BABYLON.Vector3(3829.86, 150, 2514.91), l: new BABYLON.Vector3(3830.51, 150, 2514.15)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0032", a: "PhysCamera_Laya_General_034", p: new BABYLON.Vector3(3595.79, 150, 2643.74), l: new BABYLON.Vector3(3594.82, 150, 2644)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0033", a: "PhysCamera_Laya_General_035", p: new BABYLON.Vector3(4140.41, 150, 2479.15), l: new BABYLON.Vector3(4141.1, 150, 2479.87)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0034", a: "PhysCamera_Laya_General_036", p: new BABYLON.Vector3(4689.97, 150, 2632.62), l: new BABYLON.Vector3(4690.87, 150, 2632.19)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0035", a: "PhysCamera_Laya_General_037", p: new BABYLON.Vector3(4937.59, 150, 2605.11), l: new BABYLON.Vector3(4937.71, 150, 2606.1)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0036", a: "PhysCamera_Laya_General_038", p: new BABYLON.Vector3(3843.92, 150, 2171.94), l: new BABYLON.Vector3(3843.04, 150, 2171.47)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0037", a: "PhysCamera_Laya_General_039", p: new BABYLON.Vector3(3652.36, 150, 2004.9), l: new BABYLON.Vector3(3651.91, 150, 2004.01)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0038", a: "PhysCamera_Laya_General_040", p: new BABYLON.Vector3(3593.5, 150, 1756.95), l: new BABYLON.Vector3(3592.82, 150, 1756.22)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0039", a: "PhysCamera_Laya_General_041", p: new BABYLON.Vector3(3487.15, 150, 1550.59), l: new BABYLON.Vector3(3487.51, 150, 1549.66)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0040", a: "PhysCamera_Laya_General_042", p: new BABYLON.Vector3(3519.57, 150, 1281.81), l: new BABYLON.Vector3(3519.93, 150, 1280.88)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0041", a: "PhysCamera_Laya_General_043", p: new BABYLON.Vector3(3524.47, 150, 1045.47), l: new BABYLON.Vector3(3525.44, 150, 1045.71)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0042", a: "PhysCamera_Laya_General_044", p: new BABYLON.Vector3(3629.17, 150, 1084.85), l: new BABYLON.Vector3(3630.14, 150, 1085.08)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0043", a: "PhysCamera_Laya_General_045", p: new BABYLON.Vector3(3886.75, 150, 1070.93), l: new BABYLON.Vector3(3886.06, 150, 1070.2)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0044", a: "PhysCamera_Laya_General_046", p: new BABYLON.Vector3(4007.54, 150, 1392.53), l: new BABYLON.Vector3(4008.01, 150, 1393.41)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0045", a: "PhysCamera_Laya_General_047", p: new BABYLON.Vector3(3459.76, 150, 766.971), l: new BABYLON.Vector3(3459.35, 150, 766.058)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0046", a: "PhysCamera_Laya_General_048", p: new BABYLON.Vector3(3319.63, 150, 437.298), l: new BABYLON.Vector3(3318.64, 150, 437.404)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0047", a: "PhysCamera_Laya_General_049", p: new BABYLON.Vector3(2918.56, 150, 777.214), l: new BABYLON.Vector3(2919.16, 150, 778.011)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0048", a: "PhysCamera_Laya_General_050", p: new BABYLON.Vector3(3131.28, 150, 568.818), l: new BABYLON.Vector3(3130.68, 150, 569.622)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0049", a: "PhysCamera_Laya_General_051", p: new BABYLON.Vector3(2991.23, 150, 886.338), l: new BABYLON.Vector3(2992.02, 150, 886.95)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0050", a: "PhysCamera_Laya_General_052", p: new BABYLON.Vector3(3126.44, 150, 1037.29), l: new BABYLON.Vector3(3126.76, 150, 1038.23)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0051", a: "PhysCamera_Laya_General_053", p: new BABYLON.Vector3(3354.23, 150, 109.364), l: new BABYLON.Vector3(3354.96, 150, 108.688)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0052", a: "PhysCamera_Laya_General_054", p: new BABYLON.Vector3(3625.82, 150, 116.609), l: new BABYLON.Vector3(3626.26, 150, 115.715)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0053", a: "PhysCamera_Laya_General_055", p: new BABYLON.Vector3(3700.14, 150, -91.8831), l: new BABYLON.Vector3(3701.14, 150, -91.868)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0054", a: "PhysCamera_Laya_General_056", p: new BABYLON.Vector3(3815.48, 150, -49.6168), l: new BABYLON.Vector3(3815.86, 150, -48.6947)});
+   layamaCameras.push({n: "PRUEBA PLANTA BAJA0055", a: "PhysCamera_Laya_General_057", p: new BABYLON.Vector3(3910.61, 150, 108.619), l: new BABYLON.Vector3(3910.62, 150, 109.619)});
+   return layamaCameras;
+}
+
+function getLayamaResolutions()
+{
+   var layamaResolutions = new BABYLON.SmartArray(0);
+   layamaResolutions.push("1500");
+   layamaResolutions.push("1024");
+   return layamaResolutions;
+}
+
+function getOnScreenLogoUsage()
+{
+   return 6;
+}
+
+function getLayamaControls()
+{
+   return {defMove: false, defRot: 1, altMove: true, altRot: 2};
+}
+
+function getLayamaHotspots()
+{
+   var layamaHotspots = new BABYLON.SmartArray(0);
+   layamaHotspots.push({n: "Plano Recepción", m: 0, u: "/hotspots/Plano_Recepción.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3238.27, 5, 2965.78), d: 400});
+   layamaHotspots.push({n: "Silla Recep", m: 0, u: "/hotspots/Mueble_Silla_Recepción.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3379.21, 45, 2632.42), d: 350});
+   layamaHotspots.push({n: "Mueble Recep", m: 0, u: "/hotspots/Mueble_Recepción.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3455.8, 80, 2897.57), d: 350});
+   layamaHotspots.push({n: "Det Muro Vidrio", m: 0, u: "/hotspots/Recepción-1---Detalles.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3577.99, 150, 2948.52), d: 400});
+   layamaHotspots.push({n: "Det fuente recep", m: 0, u: "/hotspots/Recepción-2---Detalles.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3445.44, 100, 2544.32), d: 400});
+   layamaHotspots.push({n: "Cromo Piscina 1", m: 0, u: "/hotspots/Piscina_cromo_01.jpg", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(4234.22, 5, 2917.95), d: 400});
+   layamaHotspots.push({n: "Cromo Piscina 2", m: 0, u: "/hotspots/Piscina_cromo_02.jpg", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3995.25, 5, 3535.67), d: 400});
+   layamaHotspots.push({n: "Plano Piscina", m: 0, u: "/hotspots/Plano_Piscina.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(4237.86, 5, 2697.77), d: 400});
+   layamaHotspots.push({n: "Det Columna piscina", m: 0, u: "/hotspots/Hidroterapia---Detalles.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3977.28, 58.5151, 3149.91), d: 400});
+   layamaHotspots.push({n: "Mueble Psicina", m: 0, u: "/hotspots/Mueble_Piscina.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3796.11, 32.9617, 3064.01), d: 400});
+   layamaHotspots.push({n: "Plano Sauna", m: 0, u: "/hotspots/Plano_Sauna.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3547.14, 5, 4480.41), d: 400});
+   layamaHotspots.push({n: "Mueble Snack", m: 0, u: "/hotspots/Mueble_snack.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(2903.58, 91.8817, 4014.99), d: 400});
+   layamaHotspots.push({n: "Plano ducha Vichy", m: 0, u: "/hotspots/Plano_duchas_Vichy.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(4928.47, 5, 2523.65), d: 400});
+   layamaHotspots.push({n: "Det Muro Verde", m: 0, u: "/hotspots/Muro-Verde---Detalles.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3910.41, 300, 2060.71), d: 400});
+   layamaHotspots.push({n: "Plano Mecano Niño", m: 0, u: "/hotspots/WhatsApp_Image_2021-12-27_at_11.44.40_AM.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3350.46, 150, 1504.41), d: 400});
+   layamaHotspots.push({n: "Plano Mecanoterapia", m: 0, u: "/hotspots/Plano_Mecanoterapia.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3950.31, 5, 1206.18), d: 400});
+   layamaHotspots.push({n: "Mueble Mecanoterapia", m: 0, u: "/hotspots/Mueble_Mecanoterapia.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3889.25, 79.9987, 1481.48), d: 400});
+   layamaHotspots.push({n: "Det Mecanoterapia", m: 0, u: "/hotspots/Mecanoterapia---Detalles.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(4085.59, 150, 1535.12), d: 400});
+   layamaHotspots.push({n: "Plano consultorio Diagnostico", m: 0, u: "/hotspots/Plano_Consultorio_Diagnóstico.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3085.95, 0, 981.399), d: 400});
+   layamaHotspots.push({n: "Plano Consultorio Fisio", m: 0, u: "/hotspots/Plano_Colsultorios_Fisiológicos.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3870.73, 0, 35.9378), d: 400});
+   layamaHotspots.push({n: "Mueble Consultorio Fisio", m: 0, u: "/hotspots/Mueble_Consultorio_Fisio.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3907.34, 77.671, 195.984), d: 400});
+   layamaHotspots.push({n: "Det Consultorio Fisio", m: 0, u: "/hotspots/Fisioterapia---Detalles.png", i: "./hotspots/InfoLogo.png", p: new BABYLON.Vector3(3846.28, 150, 328.585), d: 400});
+   return layamaHotspots;
+}
+
